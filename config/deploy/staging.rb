20d99f494fd23a17cd0ba4bd050960fc8ce2e002
Kevin Chambers <kchambers@rideconnection.org>
2015-07-27 16:57:46 -0700

    Update with new staging server info

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,8 +1,13 @@
-set :deploy_to, "/home/deployer/rails/ridepilot"
-set :branch, "master"
-set :rvm_ruby_string, '1.9.3-p550@ridepilot'
-set :rails_env, "staging"
+set :branch, 'master'
+set :rvm_ruby_version, '2.2.1@ridepilot'
+set :passenger_rvm_ruby_version, '2.2.2@passenger'
+set :deploy_to, '/home/deploy/rails/ridepilot'
+set :default_env, { "RAILS_RELATIVE_URL_ROOT" => "/ridepilot" }
 
-role :web, "184.154.158.74"
-role :app, "184.154.158.74"
-role :db,  "184.154.158.74", primary: true # This is where Rails migrations will run
+# capistrano-rails directives
+set :rails_env, 'staging'
+set :assets_roles, [:web, :app]
+set :migration_role, [:db]
+set :conditionally_migrate, true
+
+server '74.207.248.177', roles: [:app, :web, :db], user: 'deploy'
