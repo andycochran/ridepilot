e73633c085432fe45fd1408a39ca469a2a16b95c
Kevin Chambers <kchambers@rideconnection.org>
2015-07-29 16:08:36 -0700

    Update to current passenger ruby version on production server

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
 set :branch, 'stable'
 set :rvm_ruby_version, '2.2.1@ridepilot'
-set :passenger_rvm_ruby_version, '2.2.1@passenger'
+set :passenger_rvm_ruby_version, '2.2.2@passenger'
 set :deploy_to, '/home/deploy/rails/ridepilot'
 set :default_env, { "RAILS_RELATIVE_URL_ROOT" => "/ridepilot" }
 
